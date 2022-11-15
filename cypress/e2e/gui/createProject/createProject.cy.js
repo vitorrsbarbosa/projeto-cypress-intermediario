cfa2fa9b1a70a882f1fce9ee6566fa244dd6616c
Vitor Barbosa <vitorricardosilvabarbosa@gmail.com>
2022-11-15 17:19:20 -0300

    GUI test to create a project

---
+++ cypress/e2e/gui/createProject/createProject.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+const faker = require('faker');
+
+describe('Create project', () => {
+  beforeEach(() => cy.login());
+  it('Successfully', () => {
+    const project = {
+      name: `project-${faker.random.uuid()}`,
+      description: faker.random.words(5)
+    };
+    cy.gui_createProject(project);
+
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}${Cypress.env('user_name')}/${project.name}`);
+    cy.contains(project.name).should('be.visible');
+    cy.contains(project.description).should('be.visible');
+  });
+});
