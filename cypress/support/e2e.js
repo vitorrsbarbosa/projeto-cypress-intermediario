4ace765f4f867c906a414b5df328a53d85cdc594
Vitor Barbosa <vitorricardosilvabarbosa@gmail.com>
2022-11-19 13:31:59 -0300

    GUI tests - create issue

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,29 +15,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands';
+import './api_commands';
+import './gui_commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-Cypress.Commands.add('login', () => {
-  cy.visit('users/sign_in');
-
-  cy.get("[data-qa-selector='login_field']").type(Cypress.env('user_name'));
-  cy.get("[data-qa-selector='password_field']").type(Cypress.env('user_password'), { log: false });
-  cy.get("[data-qa-selector='sign_in_button']").click();
-});
-
-Cypress.Commands.add('logout', () => {
-  cy.get('.qa-user-avatar').click();
-  cy.contains('Sign out').click();
-});
-
-Cypress.Commands.add('gui_createProject', project => {
-  cy.visit('projects/new');
-
-  cy.get('#project_name').type(project.name);
-  cy.get('#project_description').type(project.description);
-  cy.get('.qa-initialize-with-readme-checkbox').check();
-  cy.contains('Create project').click();
-});
